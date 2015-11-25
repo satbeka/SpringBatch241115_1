894c2f47220283f32bf747d1763c1bf79ff5ae19
satbeka <satbeka@gmail.com>
2015-11-25 18:02:39 +0600

    241115 2

--- src/main/java/db2/SpringJdbcCreateTable.java
+++ src/main/java/db2/SpringJdbcCreateTable.java
@@ -5,12 +5,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 /**
  * Created by 1 on 25.11.2015.
  * http://www.java4s.com/spring/spring-jdbc-hello-world-example-create-table-in-database/
+ *
+ * http://www.tutorialspoint.com/spring/spring_jdbc_example.htm
  */
 public class SpringJdbcCreateTable
 {
     JdbcTemplate jt;
     String sqlCreateTbl="-- Create table\n" +
-            "create table TISR_KKB222\n" +
+            "create table tbl_name\n" +
             "(\n" +
             "  id          NUMBER not null,\n" +
             "  id_users    NUMBER,\n" +
@@ -31,14 +33,14 @@ public class SpringJdbcCreateTable
             "comment on column TISR_KKB222.create_date\n" +
             "  is 'Дата создания';\n" +
             "-- Create/Recreate primary, unique and foreign key constraints \n" +
-            "alter table TISR_KKB222\n" +
+            "alter table tbl_name\n" +
             "  add primary key (ID);\n" +
-            "alter table TISR_KKB222\n" +
+            "alter table tbl_name\n" +
             "  add constraint FK_ADM_USERS_ROLE_ROLES foreign key (ID_ROLES)\n" +
             "  references ADM_ROLES (ID) on delete cascade;\n" +
-            "alter table TISR_KKB222\n" +
+            "alter table tbl_name\n" +
             "  add constraint FK_ADM_USERS_ROLE_USERS foreign key (ID_USERS)\n" +
-            "  references TISR_KKB222 (ID) on delete cascade;";
+            "  references tbl_name (ID) on delete cascade;";
 
     public void setJt(JdbcTemplate jt)
     {
@@ -47,7 +49,11 @@ public class SpringJdbcCreateTable
 
     public void createTable(String tblName)
     {
-        jt.execute("create table tblName(sno number(3), sname varchar(10))");
+
+
+
+        getSimpleJdbcTemplate()
+        jt.execute(sqlCreateTbl);
         // execute() returns void
         System.out.println("table created");
     }
